e94c636f21f05ef04e0da56378d878e3b6e18571
Stas Stozhko <sten@quartsoft.com>
2024-10-04 22:58:53 +0300

    ДЗ №0.1 / Знайомство з JavaScript ч.1 add validate input parameter

--- extra/topic_01.js
+++ extra/topic_01.js
@@ -1,6 +1,14 @@
 function findDivisibleNumbers(n) {
     const res = [];
 
+    if (n <= 0) {
+        throw RangeError("Number must be a positive integer");
+    }
+
+    if (isNaN(n)) {
+        throw TypeError("Parameter must be a positive integer number");
+    }
+
     for (let i = 1; i <= n; i++) {
         if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
             res.push(i);
@@ -10,4 +18,5 @@ function findDivisibleNumbers(n) {
     return res;
 }
 
-console.log(findDivisibleNumbers(20));
+console.log(findDivisibleNumbers(30));
+console.log(findDivisibleNumbers(-7));
